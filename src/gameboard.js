887f0d79637dc3fc343e4d8e073edf100582dd17
Jaime <jaimeperales123@gmail.com>
2023-04-12 08:41:49 -0500

    Create board methods to manage fleet

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,27 +1,80 @@
 const Ship = require('./ship');
 
+/**
+ * Represents a gameboard that contains a 10x10 grid and fleet of ships
+ */
 class Gameboard {
   constructor() {
     this.board = [...Array(10)].map(() => Array(10));
-    this.fleet = {};
+    this.fleet = [];
   }
 
+  /**
+   * Adds ship to gameboard
+   * @param {*} shipName type of ship
+   * @param {*} length  number of hits ship can take before sunk
+   * @param {*} axis  orientation of ship (x or y)
+   * @param {*} startLocation starting cell of ship.
+   */
   addShip(shipName, length, axis, startLocation) {
     const newShip = new Ship(length);
+
+    // Horizontal ship placement
     if (axis === 'x') {
       if (newShip.length + startLocation[0] <= 9) {
+        // Checks if all cells are available for ship
+        for (let i = 0; i < newShip.length; i += 1) {
+          if (
+            this.board[startLocation[0] + i][startLocation[1]] !== undefined
+          ) {
+            return;
+          }
+        }
+        // Adds ship to board
         for (let i = 0; i < newShip.length; i += 1) {
           this.board[startLocation[0] + i][startLocation[1]] = shipName;
         }
+        // Adds ship to borad's fleet
+        this.fleet[shipName] = newShip;
       }
+
+      // Vertical ship placement
     } else if (axis === 'y') {
       if (newShip.length + startLocation[1] <= 9) {
+        // Checks if all cells are available for ship
+        for (let i = 0; i < newShip.length; i += 1) {
+          if (
+            this.board[startLocation[0]][startLocation[1] + i] !== undefined
+          ) {
+            return;
+          }
+        }
+        // Adds ship to board
         for (let i = 0; i < newShip.length; i += 1) {
           this.board[startLocation[0]][startLocation[1] + i] = shipName;
         }
+        // Adds ship to borad's fleet
         this.fleet[shipName] = newShip;
       }
     }
+  }
+
+  receiveAttack(coordinates) {
+    if (this.board[coordinates[0]][coordinates[1]] !== 'hit') {
+      if (this.board[coordinates[0]][coordinates[1]] === undefined) {
+        this.board[coordinates[0]][coordinates[1]] = 'miss';
+      } else {
+        this.fleet[this.board[coordinates[0]][coordinates[1]]].hit();
+        this.board[coordinates[0]][coordinates[1]] = 'hit';
+      }
+    }
+  }
+
+  allSunk() {
+    if (this.fleet.some((ship) => ship.sunk === false)) {
+      return false;
+    }
+    return true;
   }
 }
 module.exports = Gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,8 +1,7 @@
 import Gameboard from '../src/gameboard';
 
-const gameboard = new Gameboard();
-
 test('gameboard contains 100 cells', () => {
+  const gameboard = new Gameboard();
   let i = 0;
   for (let index = 0; index < gameboard.board.length; index += 1) {
     for (let j = 0; j < gameboard.board[index].length; j += 1) {
@@ -13,6 +12,7 @@ test('gameboard contains 100 cells', () => {
 });
 
 test('Added ship with length of 3 to board', () => {
+  const gameboard = new Gameboard();
   gameboard.addShip('destroyer', 3, 'x', [0, 0]);
   expect(gameboard.board[0][0]).toBe('destroyer');
   expect(gameboard.board[1][0]).toBe('destroyer');
@@ -20,6 +20,7 @@ test('Added ship with length of 3 to board', () => {
 });
 
 test('Added ship with length of 4 to board', () => {
+  const gameboard = new Gameboard();
   gameboard.addShip('battleship', 4, 'y', [0, 0]);
   expect(gameboard.board[0][0]).toBe('battleship');
   expect(gameboard.board[0][1]).toBe('battleship');
@@ -28,6 +29,7 @@ test('Added ship with length of 4 to board', () => {
 });
 
 test('Added ship with length of 4 to board', () => {
+  const gameboard = new Gameboard();
   gameboard.addShip('battleship', 4, 'y', [6, 7]);
   expect(gameboard.board[6][7]).toBe(undefined);
   expect(gameboard.board[6][8]).toBe(undefined);
@@ -35,8 +37,76 @@ test('Added ship with length of 4 to board', () => {
 });
 
 test('Added ship with length of 4 to board', () => {
+  const gameboard = new Gameboard();
   gameboard.addShip('battleship', 4, 'x', [7, 7]);
   expect(gameboard.board[7][7]).toBe(undefined);
   expect(gameboard.board[8][7]).toBe(undefined);
   expect(gameboard.board[9][7]).toBe(undefined);
+});
+
+test('Add ship to cell occupied by ship', () => {
+  const gameboard = new Gameboard();
+  gameboard.addShip('destroyer', 3, 'x', [0, 0]);
+  gameboard.addShip('battleship', 4, 'x', [0, 0]);
+  expect(gameboard.board[0][0]).toBe('destroyer');
+  expect(gameboard.board[1][0]).toBe('destroyer');
+  expect(gameboard.board[2][0]).toBe('destroyer');
+});
+
+test('Add ship to cell occupied by ship', () => {
+  const gameboard = new Gameboard();
+  gameboard.addShip('destroyer', 3, 'x', [0, 0]);
+  gameboard.addShip('battleship', 4, 'x', [2, 0]);
+  expect(gameboard.board[0][0]).toBe('destroyer');
+  expect(gameboard.board[1][0]).toBe('destroyer');
+  expect(gameboard.board[2][0]).toBe('destroyer');
+});
+
+test('Received attack at unoccupied cell', () => {
+  const gameboard = new Gameboard();
+  gameboard.receiveAttack([0, 1]);
+  expect(gameboard.board[0][1]).toBe('miss');
+});
+
+test('Received attack at unoccupied cell', () => {
+  const gameboard = new Gameboard();
+  gameboard.receiveAttack([0, 3]);
+  expect(gameboard.board[0][3]).toBe('miss');
+});
+
+test('Received attack at occupied cell', () => {
+  const gameboard = new Gameboard();
+  gameboard.addShip('destroyer', 3, 'x', [0, 0]);
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.board[0][0]).toBe('hit');
+  expect(gameboard.fleet.destroyer.hits).toBe(1);
+});
+
+test('Received attack at occupied cell', () => {
+  const gameboard = new Gameboard();
+  gameboard.addShip('destroyer', 3, 'x', [0, 0]);
+  gameboard.receiveAttack([0, 0]);
+  gameboard.receiveAttack([1, 0]);
+  gameboard.receiveAttack([2, 0]);
+  expect(gameboard.board[2][0]).toBe('hit');
+  expect(gameboard.fleet.destroyer.hits).toBe(3);
+  expect(gameboard.fleet.destroyer.isSunk()).toBe(true);
+});
+
+test('All ships are not sunk', () => {
+  const gameboard = new Gameboard();
+  gameboard.addShip('battleship', 4, 'y', [0, 1]);
+  gameboard.addShip('destroyer', 3, 'x', [0, 0]);
+  gameboard.fleet.battleship.sunk = true;
+  gameboard.fleet.destroyer.sunk = false;
+  expect(gameboard.allSunk() === false);
+});
+
+test('All ships are not sunk', () => {
+  const gameboard = new Gameboard();
+  gameboard.addShip('battleship', 4, 'y', [0, 1]);
+  gameboard.addShip('destroyer', 3, 'x', [0, 0]);
+  gameboard.fleet.battleship.sunk = true;
+  gameboard.fleet.destroyer.sunk = true;
+  expect(gameboard.allSunk() === true);
 });
